e1787a6c6a9e4bbbc5a020cff43942ae87de5372
duanguang <568919093@qq.com>
2017-03-10 16:41:59 +0800

    cli

---
+++ generators/app/templates/README.md
@@ -0,0 +1,20 @@
+#react-demo
+=====================
+
+如果未安装Yarn，先安装Yarn:
+
+npm install -g yarn
+
+设置Yarn的仓库为Kad私有CNPM仓库地址：
+
+yarn config set registry http://npm.360kad.com
+
+### Run the example
+
+```
+yarn install
+
+
+yarn run dev
+
+访问http://localhost:8889/
---
+++ generators/app/templates/src/entry/index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by duanguang on 2017/3/10.
+ */
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Counter from "../component/counter/Counter";
+ReactDOM.render(
+    <SignupForm ></SignupForm>,
+    document.getElementById('app')
+);
